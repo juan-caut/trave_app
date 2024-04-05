6d43fec4ae87649fbf25c8966925b9adf1249f86
juan-c <82497625+juan-caut@users.noreply.github.com>
2024-04-05 07:08:53 -0500

    detail page implemented 1.0v

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:trave_app/misc/colors.dart';
+import 'package:trave_app/widgets/app_buttons.dart';
 import 'package:trave_app/widgets/app_large_text.dart';
 import 'package:trave_app/widgets/app_text.dart';
+import 'package:trave_app/widgets/responsive_button.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -11,6 +14,8 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  int gotttenStars = 4;
+  int selectedIndex = -1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,82 +24,188 @@ class _DetailPageState extends State<DetailPage> {
         height: double.maxFinite,
         child: Stack(
           children: [
-            
             Positioned(
-              left: 0,
-              right: 0,
-              child: Container(
-                width: double.maxFinite,
-                height: 350,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("img/mountain.jpg"),
-                    fit:BoxFit.cover,
+                left: 0,
+                right: 0,
+                child: Container(
+                  width: double.maxFinite,
+                  height: 350,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("img/mountain.jpg"),
+                      fit: BoxFit.cover,
                     ),
-                ),
-            )
-            ),
-
+                  ),
+                )),
             Positioned(
-              left: 20,
-              top: 70,
-              child: Row(
-              children: [
-                IconButton(onPressed: (){}, icon: Icon(Icons.menu),
-                color: Colors.white,
-                )
-              ],
-            )
-            ),
-            
+                left: 20,
+                top: 70,
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.menu),
+                      color: Colors.white,
+                    )
+                  ],
+                )),
             Positioned(
               top: 320,
               child: Container(
-                padding: const EdgeInsets.only(left: 20, right: 20,top: 30),
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
                 width: MediaQuery.of(context).size.width,
                 height: 500,
                 decoration: BoxDecoration(
-                  color:Colors.white,
-                  borderRadius:BorderRadius.only(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(30),
                     topRight: Radius.circular(30),
                   ),
                 ),
-                child:Column(
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        AppLargeText(text: "Yosemite", color: Colors.black.withOpacity(0.8),),
-                        AppLargeText(text: "\$ 250",color: AppColors.mainColor,),
+                        AppLargeText(
+                          text: "Yosemite",
+                          color: Colors.black.withOpacity(0.8),
+                        ),
+                        AppLargeText(
+                          text: "\$ 250",
+                          color: AppColors.mainColor,
+                        ),
                       ],
                     ),
-                    SizedBox(height: 10,),
+                    SizedBox(
+                      height: 10,
+                    ),
                     Row(
                       children: [
-                        Icon(Icons.location_on,color: AppColors.mainColor,),
-                        SizedBox(width: 5,),
-                        AppText(text: "USA, California", color: AppColors.textColor1)
+                        Icon(
+                          Icons.location_on,
+                          color: AppColors.mainColor,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        AppText(
+                            text: "USA, California",
+                            color: AppColors.textColor1)
                       ],
                     ),
-                    SizedBox(height: 20,),
+                    SizedBox(
+                      height: 20,
+                    ),
                     Row(
                       children: [
                         Wrap(
-                          children: List.generate(5, (index){
-                            return Icon(Icons.star,color:AppColors.starColor);
-                          },)
+                            children: List.generate(
+                          5,
+                          (index) {
+                            return Icon(Icons.star,
+                                color: index < gotttenStars
+                                    ? AppColors.starColor
+                                    : AppColors.textColor2);
+                          },
+                        )),
+                        SizedBox(
+                          width: 10,
                         ),
-                        SizedBox(width: 10,),
-                        AppText(text: "(4.0)",color: AppColors.textColor2,)
+                        AppText(
+                          text: "(4.0)",
+                          color: AppColors.textColor2,
+                        )
                       ],
                     ),
-
+                    SizedBox(
+                      height: 25,
+                    ),
+                    AppLargeText(
+                        text: "People",
+                        color: Colors.black.withOpacity(0.8),
+                        size: 20),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    AppText(
+                      text: "Number of people in your group",
+                      color: AppColors.mainTextColor,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Wrap(
+                      children: List.generate(5, (index) {
+                        return InkWell(
+                          onTap: () {
+                            setState(() {
+                              selectedIndex = index;
+                            });
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(right: 10),
+                            child: AppButtons(
+                              size: 50,
+                              color: selectedIndex == index
+                                  ? Colors.white
+                                  : Colors.black,
+                              backgroudColor: selectedIndex == index
+                                  ? Colors.black
+                                  : AppColors.buttonBackground,
+                              borderColor: selectedIndex == index
+                                  ? Colors.black
+                                  : AppColors.buttonBackground,
+                              text: (index + 1).toString(),
+                              //icon: Icons.favorite_border,
+                              //isIcon: true,
+                            ),
+                          ),
+                        );
+                      }),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    AppLargeText(
+                      text: "Description ",
+                      color: Colors.black.withOpacity(0.8),
+                      size: 20,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    AppText(
+                      text:
+                          "You must go fot travel. Travelling helps get rid of pressure. Got to the mountains to see the nature.",
+                      color: AppColors.mainTextColor,
+                    )
                   ],
                 ),
               ),
             ),
+            Positioned(
+                bottom: 20,
+                left: 20,
+                right: 20,
+                child: Row(
+                  children: [
+                    AppButtons(
+                        color: AppColors.textColor1,
+                        backgroudColor: Colors.white,
+                        size: 60,
+                        borderColor: AppColors.textColor1,
+                        isIcon: true,
+                        icon: Icons.favorite_border),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    ResponsiveButton(
+                      isResponsive: true,
+                    )
+                  ],
+                ))
           ],
         ),
       ),
--- lib/widgets/responsive_button.dart
+++ lib/widgets/responsive_button.dart
@@ -1,32 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:trave_app/misc/colors.dart';
+import 'package:trave_app/widgets/app_text.dart';
 
 class ResponsiveButton extends StatelessWidget {
   bool? isResponsive;
   double? width;
-  ResponsiveButton({Key? key, this.width, this.isResponsive=false}):super(key: key);
+  ResponsiveButton({Key? key, this.width, this.isResponsive = false})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,
+    return Flexible(
+        child: Container(
+      width: isResponsive == true ? double.maxFinite : width,
       height: 60,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
         color: AppColors.mainColor,
       ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Next",style: TextStyle(
-            color:Colors.white60,
-            fontSize: 15,
-            fontWeight: FontWeight.bold
-            ),
-            ),
-          //Image.asset("img/button-one.png")
-        ]),
-    );
-    
+          mainAxisAlignment: isResponsive == true
+              ? MainAxisAlignment.center
+              : MainAxisAlignment.center,
+          children: [
+            isResponsive == true
+                ? AppText(text: "book trip now", color: Colors.white)
+                : Text(
+                    "Next",
+                    style: TextStyle(
+                        color: Colors.white60,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
+                  ),
+            /*
+            Text(
+              "Next",
+              style: TextStyle(
+                  color: Colors.white60,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            ),*/
+            //Image.asset("img/button-one.png")
+          ]),
+    ));
   }
 }
