afc5ec374a8d0b2c6ab7cd05d506e9af81fc68cd
juan-c <82497625+juan-caut@users.noreply.github.com>
2024-04-12 22:33:54 -0500

    final course code

--- lib/cubit/app_cubit_logics.dart
+++ lib/cubit/app_cubit_logics.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:trave_app/cubit/app_cubit_states.dart';
 import 'package:trave_app/cubit/app_cubits.dart';
+import 'package:trave_app/pages/detail_page.dart';
 import 'package:trave_app/pages/home_page.dart';
+import 'package:trave_app/pages/navpages/main_page.dart';
 import 'package:trave_app/pages/welcome_page.dart';
 
 class AppCubitLogics extends StatefulWidget {
@@ -18,11 +20,15 @@ class _AppCubitLogicsState extends State<AppCubitLogics> {
     return Scaffold(
       body: BlocBuilder<AppCubits, CubitStates>(
         builder: (context, state) {
+          if (state is DetailState) {
+            //03:53:12 video youtube
+            return DetailPage();
+          }
           if (state is WelcomeState) {
             return WelcomePage();
           }
           if (state is LoadedState) {
-            return HomePage();
+            return MainPage();
           }
           if (state is LoadingState) {
             return Center(
